c3e4a89deef406c17ea3f15190a3c4319c957218
Michael de Selincourt <m.de.selincourt@googlemail.com>
2021-02-06 16:59:06 +0000

    apikey separate

--- weather-poller.py
+++ weather-poller.py
@@ -1,7 +1,9 @@
 import json, time
 
+apikey = open("apikey.txt", "r").read()
+
 import urllib.request
-with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?id=2643743&appid=") as response:
+with urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?id=2643743&appid={apikey}".format(apikey=apikey)) as response:
     print(str(response))
     weather = json.loads(response.read().decode())
     wind = weather["wind"]
